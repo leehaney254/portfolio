31b3f58b2d0e24807edbedac5b3441d0df405e62
Leehaney gee <georgeleehaney20@gmail.com>
2023-06-01 08:21:12 +0300

    Feat: Create pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Intro from '../components/Intro';
+import About from '../components/About';
+import Projects from '../components/Projects';
+import Posts from '../components/Posts';
 
 const Home = () => {
   return (
     <main>
       <Intro />
+      <About />
+      <Projects />
+      <Posts />
     </main>
   )
 }
