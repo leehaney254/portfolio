2b210be19bee4ef5474398fbcdc2555ed5991d12
Leehaney gee <georgeleehaney20@gmail.com>
2023-06-01 08:21:47 +0300

    Feat: Add data

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,7 +1,57 @@
+import capstone from '../assets/tutors.png';
+import { FaLinkedinIn, FaGithub, FaTwitter } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
 export const name = 'Leehaney Gerorge';
 
 export const profession = "I'm a Software Developer";
 
 export const catchPhrase = "Crafting code with passion and precision. If you like what you see, don't hesitate to contact me.";
 
-export const linkedin = 'https://www.linkedin.com/in/leehaney-george/';
+export const socilaMedia = [
+  {
+    link: 'https://www.linkedin.com/in/leehaney-george/',
+    icon: FaLinkedinIn,
+  },
+  {
+    link: 'https://github.com/leehaney254',
+    icon: FaGithub,
+  },
+  {
+    link: 'https://twitter.com/Lee06785586',
+    icon: FaTwitter,
+  },
+  {
+    link: 'mailto:georgeleehaney20@gmail.com',
+    icon: SiGmail,
+  },
+];
+
+export const aboutParagraph = "Allow me to introduce myself—a driven and dedicated junior software developer with a passion for crafting exceptional web applications. Equipped with a degree in mechatronics engineering and armed with a full-stack web developer certificate from Microverse, I thrive on the challenges and opportunities that arise in this ever-evolving field. Collaborating with diverse individuals from around the globe, I have honed my skills in developing solution-oriented applications that make a tangible impact. Not only am I a proficient team player, but I also possess strong leadership abilities. Beyond coding, you'll find me indulging in my adventurous side, exploring nature through invigorating hikes, pushing my limits at the gym, or delving into captivating documentary series. With an insatiable appetite for growth and innovation, I am eager to contribute my expertise and enthusiasm to create remarkable digital experiences.";
+
+export const Project = [
+  {
+    image: capstone,
+    title: 'Tutors Appointment',
+    description: '🌟 Seeking Quality Education? Look No Further! 📚✨ 🔥 Introducing Tutors-appointment! 🎓 Your ultimate destination for booking top-notch tutors tailored to your learning needs! With authentication and authorization implimented privilages and roles have been easily assigned to users and administrators.💪',
+    technologies: ['React js', 'Redux', 'Tailwind', 'Ruby on Rails', 'Postgresql'],
+    live: 'https://starlit-syrniki-d3f2fe.netlify.app/login',
+    github: 'https://github.com/leehaney254/tutors-appointment-frontend',
+  },
+  {
+    image: capstone,
+    title: 'Tutors Appointment',
+    description: '🌟 Seeking Quality Education? Look No Further! 📚✨ 🔥 Introducing Tutors-appointment! 🎓 Your ultimate destination for booking top-notch tutors tailored to your learning needs! With authentication and authorization implimented privilages and roles have been easily assigned to users and administrators.💪',
+    technologies: ['React js', 'Redux', 'Tailwind', 'Ruby on Rails', 'Postgresql'],
+    live: 'https://starlit-syrniki-d3f2fe.netlify.app/login',
+    github: 'https://github.com/leehaney254/tutors-appointment-frontend',
+  },
+  {
+    image: capstone,
+    title: 'Tutors Appointment',
+    description: '🌟 Seeking Quality Education? Look No Further! 📚✨ 🔥 Introducing Tutors-appointment! 🎓 Your ultimate destination for booking top-notch tutors tailored to your learning needs! With authentication and authorization implimented privilages and roles have been easily assigned to users and administrators.💪',
+    technologies: ['React js', 'Redux', 'Tailwind', 'Ruby on Rails', 'Postgresql'],
+    live: 'https://starlit-syrniki-d3f2fe.netlify.app/login',
+    github: 'https://github.com/leehaney254/tutors-appointment-frontend',
+  },
+];
