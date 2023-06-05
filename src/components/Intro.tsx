a8f8044501d24a07d83b880cfae2a858cfe1e80e
Leehaney gee <georgeleehaney20@gmail.com>
2023-06-05 17:31:20 +0300

    Docs: Intro image

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Spin as Hamburger } from 'hamburger-react'
 import { name, profession, catchPhrase, socilaMedia } from '../data/data';
+import imageIntro from '../assets/intro.png';
 
 const Intro: React.FC = () => {
   const toogleMenu = (toogled: any) => {
@@ -12,15 +13,17 @@ const Intro: React.FC = () => {
   }
 
   return (
-    <section className="p-4 bg-black text-white h-screen">
-      <div className="flex justify-end">
+    <section className="p-4 bg-black text-white h-screen flex flex-col gap-6 md:items-center md:justify-center md: lg:flex-row">
+      <div className="flex justify-end md:hidden">
         <Hamburger onToggle={toogleMenu} />
       </div>
-      <div className="mt-40 flex flex-col gap-1">
-        <h1 className="text-3xl font-bold">{name}</h1>
-        <p className="text-xl">{profession}</p>
-        <p className="text-lg">{catchPhrase}</p>
-        <div className="flex flex-row gap-2">
+      <div className="mt-38 flex flex-col gap-1 md:flex-row-reverse md:gap-4 md:mt-0">
+        <div>
+          <h1 className="text-3xl font-bold">{name}</h1>
+          <p className="text-xl">{profession}</p>
+          <p className="text-lg">{catchPhrase}</p>
+        </div>
+        <div className="flex flex-row gap-2 md:flex-col">
           {
             socilaMedia.map((element, index) => {
               return (
@@ -29,6 +32,9 @@ const Intro: React.FC = () => {
             })
           }
         </div>
+      </div>
+      <div>
+        <img className="w-96 h-96" src={imageIntro} alt="dev image" />
       </div>
     </section>
   )
