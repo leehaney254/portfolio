5fd9335938057c6bbe363bcc7947de2d2e31ef6d
Leehaney gee <georgeleehaney20@gmail.com>
2023-06-05 18:34:26 +0300

    Feat: Add message image

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,6 +1,7 @@
-import React, { ChangeEvent } from 'react'
+import React from 'react'
 import { socilaMedia } from '../data/data';
 import emailjs from 'emailjs-com';
+import massageImg from '../assets/message.png';
 
 const Message: React.FC = () => {
   const sendEmail = (e: any) => {
@@ -18,52 +19,55 @@ const Message: React.FC = () => {
       <div className="flex flex-col gap-2 items-center mt-8">
         <p className="text-green-600">LETS CHAT!</p>
         <h2 className="text-xl font-bold text-center mb-10">Got questions or proposal, or just want to say hello? Go ahead</h2>
-        <form onSubmit={sendEmail}>
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
-              Your Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="inputborder bg-black rounded-md px-4 py-2 w-full"
-              required
-            />
-          </div>
+        <div className="md:flex gap-24">
+          <form onSubmit={sendEmail}>
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
+                Your Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="inputborder bg-black rounded-md px-4 py-2 w-full"
+                required
+              />
+            </div>
 
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="inputborder bg-black rounded-md px-4 py-2 w-full"
-              required
-            />
-          </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="inputborder bg-black rounded-md px-4 py-2 w-full"
+                required
+              />
+            </div>
 
-          <div className="mb-4">
-            <label htmlFor="message" className="block text-gray-700 font-bold mb-2">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="inputborder bg-black rounded-md px-4 py-2 w-full"
-              required
-            ></textarea>
-          </div>
+            <div className="mb-4">
+              <label htmlFor="message" className="block text-gray-700 font-bold mb-2">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="inputborder bg-black rounded-md px-4 py-2 w-full"
+                required
+              ></textarea>
+            </div>
 
-          <button
-            type="submit"
-            className="bg-green-500 text-white font-bold py-2 px-4 rounded"
-          >
-            Submit
-          </button>
-        </form>
+            <button
+              type="submit"
+              className="bg-green-500 text-white font-bold py-2 px-4 rounded"
+            >
+              Submit
+            </button>
+          </form>
+          <img className="hidden md:block w-96 h-96" src={massageImg} alt="message" />
+        </div>
         <div className="flex flex-row gap-2 mt-10">
           {
             socilaMedia.map((element, index) => {
