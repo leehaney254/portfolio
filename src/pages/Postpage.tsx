3be509220078c73b026c4108af47ae945dbdf981
Leehaney gee <georgeleehaney20@gmail.com>
2023-06-07 11:06:18 +0300

    Feat: Limit number of posts seen

--- src/pages/Postpage.tsx
+++ src/pages/Postpage.tsx
@@ -11,22 +11,24 @@ const Postpage: React.FC = () => {
           <BiArrowBack className="text-3xl" />
         </Link>
       </header>
-      <h1 className="text-3xl font-bold my-7 text-gray-700">Writing</h1>
-      <section className="flex flex-col gap-10">
-        {
-          posts.slice(3).map((article, index) => {
-            return (
-              <div key={index}>
-                <h2 className="text-3xl font-semibold text-green-400">{article.heading}</h2>
-                <div className="flex gap-2 my-2 text-gray-400">
-                  <p>{article.date}</p>
-                  <p>{article.genre}</p>
+      <section className="flex flex-col md:items-center">
+        <h1 className="text-3xl font-bold my-7 text-gray-700">Writing</h1>
+        <div className="flex flex-col gap-10">
+          {
+            posts.slice(3).map((article, index) => {
+              return (
+                <div key={index}>
+                  <h2 className="text-3xl font-semibold text-green-400">{article.heading}</h2>
+                  <div className="flex gap-2 my-2 text-gray-400">
+                    <p>{article.date}</p>
+                    <p>{article.genre}</p>
+                  </div>
+                  <p className="text-lg">{article.description}</p>
                 </div>
-                <p className="text-lg">{article.description}</p>
-              </div>
-            )
-          })
-        }
+              )
+            })
+          }
+        </div>
       </section>
     </main>
   )
