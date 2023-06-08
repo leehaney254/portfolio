795794082bd21f56603e6a70fc2952f2846e6755
Leehaney gee <georgeleehaney20@gmail.com>
2023-06-08 11:02:14 +0300

    Feat: update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import { Link } from 'react-scroll';
 import { Spin as Hamburger } from 'hamburger-react';
 import { menu } from '../data/data';
 
