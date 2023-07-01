ebbf0d394973e3a1608441f0469ed0e4fabb362b
Leehaney gee <georgeleehaney20@gmail.com>
2023-07-01 10:13:19 +0300

    Sty;e fix icon position

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -7,7 +7,7 @@ import './intro.css'
 const Intro: React.FC = () => {
   return (
     <section id="Intro" className="p-4 mt-10 md:mt-0 bg-black text-white h-screen flex flex-col gap-6 md:items-center md:justify-center md: lg:flex-row">
-      <div className="mt-38 flex flex-col gap-1 md:flex-row-reverse md:gap-4 md:mt-0 introanime">
+      <div className="mt-38 flex flex-col gap-1 md:flex-row-reverse md:items-center md:gap-4 md:mt-0 introanime">
         <div>
           <h1 className="text-3xl font-bold lg:text-5xl">{name}</h1>
           <p className="text-xl">{profession}</p>
